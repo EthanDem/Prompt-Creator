2f8246a9df1e0d97157af39a34ac9b8759c65582
EthanDem <113375436+EthanDem@users.noreply.github.com>
2023-06-27 12:40:31 -0700

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import json
+
+def create_prompt(title, content):
+    return {"title": title, "prompt": content}
+
+def create_json_sequence(name, prompts):
+    return {"name": name, "prompts": prompts}
+
+def download_link(object_to_download, download_filename, download_link_text):
+    import base64
+    import pandas as pd
+    if isinstance(object_to_download,pd.DataFrame):
+        object_to_download = object_to_download.to_csv(index=False)
+
+    # some strings <-> bytes conversions necessary here
+    b64 = base64.b64encode(object_to_download.encode()).decode()
+
+    return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
+
+def app():
+    st.title("JSON Prompt Creator")
+
+    st.header("Upload existing JSON file")
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+        data = json.load(uploaded_file)
+    else:
+        data = []
+
+    st.header("Add new prompt sequence")
+    sequence_name = st.text_input("Enter sequence name")
+    number_of_prompts = st.slider("Number of prompts in the sequence", 1, 10)
+
+    prompts = []
+    for i in range(number_of_prompts):
+        st.subheader(f"Prompt {i+1}")
+        title = st.text_input(f"Prompt title {i+1}")
+        content = st.text_input(f"Prompt content {i+1}")
+        prompts.append(create_prompt(title, content))
+
+    if st.button("Add Sequence"):
+        data.append(create_json_sequence(sequence_name, prompts))
+
+    if st.button("Download JSON file"):
+        json_data = json.dumps(data, indent=4)
+        tmp_download_link = download_link(json_data, 'prompt.json', 'Click here to download your JSON file')
+        st.markdown(tmp_download_link, unsafe_allow_html=True)
+
+app()
